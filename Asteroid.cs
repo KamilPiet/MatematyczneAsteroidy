c0b5528887e91a1131557f2afbbe6a43f6323d5c
KamilPiet <kamil.piet@gmail.com>
2018-12-02 23:15:34 +0100

    Small fixes

--- Asteroid.cs
+++ Asteroid.cs
@@ -9,18 +9,27 @@ namespace MatematyczneAsteroidy
 {
     class Asteroid : ImageBase
     {
-        public int xVel;
-        public int yVel;
+        public double xVel = 0.1;
+        public double yVel = -0.1;
 
         private Rectangle _AsteroidBox = new Rectangle();
 
         public Asteroid(Bitmap _h)
             : base(_h)
         {
-            this._AsteroidBox.X = Left + 10;
-            this._AsteroidBox.Y = Top + 10;
-            this._AsteroidBox.Width = 130;
-            this._AsteroidBox.Height = 130;
+            this._AsteroidBox.X = Left + 4;
+            this._AsteroidBox.Y = Top + 4;
+            this._AsteroidBox.Width = 52;
+            this._AsteroidBox.Height = 52;
+        }
+        public void Update(double X, double Y)
+        {
+            this.Left = (int)Math.Round(X);
+            this.Top = (int)Math.Round(Y);
+            this._AsteroidBox.X = Left + 4;
+            this._AsteroidBox.Y = Top + 4;
+            this._AsteroidBox.Width = 52;
+            this._AsteroidBox.Height = 52;
         }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,43 +16,41 @@ namespace MatematyczneAsteroidy
         Bitmap _h;
         SpaceShip SpaceShip;
         List<Asteroid> asteroids = new List<Asteroid>();
-        //Asteroid Asteroid;
-        int numberOfAsteroids = 3;
+        int numberOfAsteroids = 30;
         public Form1()
         {
             InitializeComponent();
-            switch (rnd.Next(1, 3))
+            switch (rnd.Next(0, 2))
             {
-                case 1:
+                case 0:
                     _h = new Bitmap(Properties.Resources.Ast1);
                     break;
-                case 2:
+                case 1:
                     _h = new Bitmap(Properties.Resources.Ast2);
                     break;
-                case 3:
+                case 2:
                     _h = new Bitmap(Properties.Resources.Ast3);
                     break;
             }
             SpaceShip = new SpaceShip() { Left = this.Width/2, Top = this.Height/2};
             for (int i = 0; i < numberOfAsteroids; i++)
-                asteroids.Add(new Asteroid(_h) { Left = rnd.Next(), Top = rnd.Next() });
-           // Asteroid = new Asteroid(_h) { Left = rnd.Next(0, this.Width), Top = rnd.Next(0, this.Height) };
+                asteroids.Add(new Asteroid(_h) { Left = rnd.Next(0, this.Width), Top = rnd.Next(0, this.Height) });
         }
 
         private void timerGameLoop_Tick(object sender, EventArgs e)
         {
             //SpaceShip.Update(100, 100);
-            //this.Refresh();
+            foreach (Asteroid a in asteroids)
+                a.Update(a.Left + a.xVel * timerGameLoop.Interval, a.Top + a.yVel * timerGameLoop.Interval);
+            this.Refresh();
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             Graphics g = e.Graphics;
             SpaceShip.DrawImage(g);
-            //foreach(Asteroid a in asteroids)
-            //a.DrawImage(g);
-            asteroids[1].DrawImage(g);
-            //Asteroid.DrawImage(g);
+            foreach(Asteroid a in asteroids)
+                a.DrawImage(g);
             base.OnPaint(e);
         }
 
